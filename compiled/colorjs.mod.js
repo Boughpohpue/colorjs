bbc4fc13588c3bbbd7e7753387dc5cc482a7f424
Boughpohpue <joseph.marianson@gmail.com>
2026-03-16 20:15:19 +0100

    Added HSL methods to ColorHelper

--- compiled/colorjs.mod.js
+++ compiled/colorjs.mod.js
@@ -1,6 +1,6 @@
 import Enum from 'https://boughpohpue.github.io/enum.js/content/enum.mod.js'
 
-export class ColorHelper {
+class ColorHelper {
   static isHexColor(s) {
     return s.match(/^#?([A-F0-9]{3,4}|[A-F0-9]{6}|[A-F0-9]{8})$/i) !== null;
   }
@@ -66,6 +66,60 @@ export class ColorHelper {
 			? channelFrom + Math.round(Math.floor((channelTo - channelFrom) * transit))
 			: channelFrom - Math.round(Math.floor((channelFrom - channelTo) * transit));
 	}
+  static getContrastColor(hex) {
+    [r,g,b] = ColorHelper.hex2rgb(hex);
+    const luminance = (0.2126 * r) + (0.7152 * g) + (0.0722 * b);
+    return luminance > 140 ? "#000000" : "#FFFFFF";
+  }
+  static invertHsl(h, s, l) {
+    return [(h + 180) % 360, s, 1 - l];
+  }
+  static rgb2hsl(r, g, b) {
+    const max = Math.max(r,g,b);
+    const min = Math.min(r,g,b);
+    let h, s, l = (max+min)/2;
+    if (max === min) { h = s = 0; }
+    else {
+      const d = max - min;
+      s = l > 0.5 ? d/(2-max-min) : d/(max+min);
+      switch(max){
+        case r: h=(g-b)/d+(g<b?6:0); break;
+        case g: h=(b-r)/d+2; break;
+        case b: h=(r-g)/d+4; break;
+      }
+      h/=6;
+    }
+    return [h*360, s, l];
+  }
+
+  static hsl2rgb(h, s, l) {
+    const c = (1 - Math.abs(2 * l - 1)) * s;
+    const x = c * (1 - Math.abs((h / 60) % 2 - 1));
+    const m = l - c / 2;
+    let r = 0, g = 0, b = 0;
+
+    if (h < 60)       [r,g,b] = [c,x,0];
+    else if (h < 120) [r,g,b] = [x,c,0];
+    else if (h < 180) [r,g,b] = [0,c,x];
+    else if (h < 240) [r,g,b] = [0,x,c];
+    else if (h < 300) [r,g,b] = [x,0,c];
+    else              [r,g,b] = [c,0,x];
+    return [
+      Math.round((r + m) * 255),
+      Math.round((g + m) * 255),
+      Math.round((b + m) * 255)
+    ];
+  }
+  static hsl2rgb2(h, s, l) {
+    const k = n => (n + h / 30) % 12;
+    const a = s * Math.min(l, 1 - l);
+    const f = n => l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+    return [
+      Math.round(255 * f(0)),
+      Math.round(255 * f(8)),
+      Math.round(255 * f(4))
+    ];
+  }
   static normalizeHex(hex) {
     hex = hex.startsWith('#') ? hex.substring(1) : hex;
     if (hex.length === 3 || hex.length === 4)
